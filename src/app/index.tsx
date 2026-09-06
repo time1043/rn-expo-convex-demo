02e37f7f0d353c99a27ba837401e4b279c6ecc43
oswin-m4a <132178516+time1043@users.noreply.github.com>
2026-09-07 00:38:30 +0800

    chore(setup:convex): query demo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,20 @@
-import { Text, View, StyleSheet } from "react-native";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { Text, View } from "react-native";
 
 export default function Index() {
+  const tasks = useQuery(api.tasks.get);
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {tasks?.map(({ _id, text }) => (
+        <Text key={_id}>{text}</Text>
+      ))}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
